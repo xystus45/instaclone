29cce5ea078fb5b1be29db21eb52673ff9a2e5ae
xystus45 <xystusngigi@gmail.com>
2019-09-03 08:27:59 +0300

    restart

--- photos/urls.py
+++ photos/urls.py
@@ -5,15 +5,15 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='landing'),
-    path('myaccount/', views.mine, name='myaccount'),
-    path('myaccount/edit/', views.edit, name='edit'),
-    path('comment/<int:post_id>', views.comment_on, name='comment'),
-    path('user/(?P<user_id>\d+)', views.user, name='aboutuser'),
-    path('like/(?P<post_id>\d+)', views.like, name='like'),
-    path('save/(?P<post_id>\d+)', views.save, name='save'),
-    path('search/(?P<name>.+)', views.find, name='save'),
-    path('follow_or_not/(?P<user_id>\d+)', views.togglefollow, name='follow_or_not'),
-    path('unlike/(?P<post_id>\d+)', views.unlike, name='unlike')
+    path('^myaccount/', views.mine, name='myaccount'),
+    path('^myaccount/edit/', views.edit, name='edit'),
+    path('^comment/<int:post_id>', views.comment_on, name='comment'),
+    path('^user/(?P<user_id>\d+)', views.user, name='aboutuser'),
+    path('^like/(?P<post_id>\d+)', views.like, name='like'),
+    path('^save/(?P<post_id>\d+)', views.save, name='save'),
+    path('^search/(?P<name>.+)', views.find, name='save'),
+    path('^follow_or_not/(?P<user_id>\d+)', views.togglefollow, name='follow_or_not'),
+    path('^unlike/(?P<post_id>\d+)', views.unlike, name='unlike')
 ]
 
 if settings.DEBUG:
